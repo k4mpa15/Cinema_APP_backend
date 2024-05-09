102c4ae35754ab5b8c466236e65ce1f48d40fd17
k4mpa15 <kampa.kampa@wp.pl>
2024-05-09 15:29:42 +0200

    first endpoint

--- src/main/java/pl/wat/backend/controller/PricesController.java
+++ src/main/java/pl/wat/backend/controller/PricesController.java
@@ -1,17 +1,19 @@
-package pl.wat.backend;
+package pl.wat.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.wat.backend.Greeting;
+import pl.wat.backend.GreetingService;
 
 @RestController
-public class GreetingController {
+public class PricesController {
 
     @Autowired
     private GreetingService greetingService;
 
-    @GetMapping("/greeting")
+    @GetMapping("/prices/giftcard")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return greetingService.generateGreeting(name);
     }
